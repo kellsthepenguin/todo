0ddca922fd36e08d09dc3827b098517896dd7a9e
Kim Jioo <jwkim101201@gmail.com>
2020-11-19 17:13:40 +0900

    yea..

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,10 @@ const crypto = require('crypto')
 
 window.onload = init
 
+document.getElementById('clear').addEventListener('click', () => {
+  clear()
+})
+
 function init () {
   if (!isItemExists()) {
     /*
@@ -16,33 +20,84 @@ function init () {
           id: 'randomid'
         }
       */
-    localStorage.setItem('data', '{ "todos": [], "canceled": [] }')
+    localStorage.setItem('data', '{ "todos": [] }')
   }
+
+  loadTodos()
+}
+
+function loadTodos () {
+  const ulTodos = document.getElementById('todos')
+  const data = JSON.parse(localStorage.data)
+  const todos = data.todos
+
+  while (ulTodos.hasChildNodes()) {
+    ulTodos.removeChild(ulTodos.firstChild)
+  }
+
+  todos.forEach(elem => {
+    const li = document.createElement('li')
+
+    if (elem.canceled) {
+      li.innerHTML = `
+      <del title="${elem.description}">
+        ${elem.name}
+      </del>
+      `
+    } else {
+      li.innerHTML = `
+      <span title="${elem.description}">
+        ${elem.name}
+      </span>
+      `
+    }
+    li.onclick = () => {
+      setTodoCanceled(elem.id, !elem.canceled)
+    }
+
+    ulTodos.appendChild(li)
+  })
 }
 
 /**
  * @returns {boolean} is item exists
 */
 function isItemExists () {
-  return localStorage.getItem('data') != null
+  return localStorage.data !== undefined
 }
 
 /**
- * 
+ *
  * @param {string} name
  * @param {string} description
  * @param {string} date
  */
 function addTodo (name, description, date) {
-    const data = JSON.parse(localStorage.data)
-    const obj = {
-        name: name,
-        description: description,
-        date: date,
-        id: crypto.randomBytes(16).toString('hex')
-    }
+  const data = JSON.parse(localStorage.data)
+  const obj = {
+    name: name,
+    description: description,
+    date: date,
+    id: crypto.randomBytes(3).toString('hex'),
+    canceled: false
+  }
 
-    data.todos.concat(obj)
+  data.todos.push(obj)
+  localStorage.setItem('data', JSON.stringify(data))
+  loadTodos()
+}
 
-    localStorage.setItem('data', data)
+function setTodoCanceled (id, canceled) {
+  const data = JSON.parse(localStorage.data)
+  const todos = data.todos
+
+  todos.find(elem => elem.id === id).canceled = canceled
+
+  localStorage.setItem('data', JSON.stringify(data))
+  loadTodos()
+}
+
+function clear () {
+  localStorage.data = '{ "todos": [] }'
+  loadTodos()
 }
